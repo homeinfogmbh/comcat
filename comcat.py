1bb2cee8bcd027abc2e7bb71ca05eed2de680a80
Richard Neumann <r.neumann@homeinfo.de>
2019-01-31 10:39:13 +0100

    Implemented facebook posts.

--- comcat.py
+++ comcat.py
@@ -8,10 +8,15 @@ from flask import request, Flask
 from cmslib.presentation.comcat_account import Presentation
 from comcatlib import ACCOUNT
 from comcatlib import Account
+from comcatlib import AccountDamageReport
 from comcatlib import Session
 from comcatlib import authenticated
+from comcatlib import decode_url
+from comcatlib import get_facebook_posts as _get_facebook_posts
 from comcatlib import get_session_duration
+from comcatlib import proxy_url
 from comcatlib.messages import INVALID_CREDENTIALS
+from damage_report import DamageReport
 from wsgilib import JSON
 
 
@@ -58,3 +63,44 @@ def get_presentation():
 
     json['sha256sum'] = sha256sum
     return JSON(json)
+
+
+@APPLICATION.route('/facebook', methods=['GET'])
+@authenticated
+def get_facebook_posts():
+    """Returns a list of sent damage report."""
+
+    facebook_posts = _get_facebook_posts(ACCOUNT)
+    return JSON([facebook_post.to_json() for facebook_post in facebook_posts])
+
+
+@APPLICATION.route('/facebook/image', methods=['POST'])
+@authenticated
+def get_facebook_image():
+    """Returns the respective facebook image."""
+
+    url = decode_url(request.json)
+    return proxy_url(url)
+
+
+@APPLICATION.route('/damage_report', methods=['GET'])
+@authenticated
+def list_damage_reports():
+    """Returns a list of sent damage report."""
+
+    damage_reports = DamageReport.select().where(
+        DamageReport.account == ACCOUNT.id)
+    return JSON([damage_report.to_dict() for damage_report in damage_reports])
+
+
+@APPLICATION.route('/damage_report', methods=['POST'])
+@authenticated
+def submit_damage_report():
+    """Submits a new damage report."""
+
+    damage_report = DamageReport.from_json(
+        request.json, ACCOUNT.customer, ACCOUNT.address)
+    damage_report.save()
+    account_damage_report = AccountDamageReport(ACCOUNT.id, damage_report)
+    account_damage_report.save()
+    return ('Damage report submitted.', 201)
