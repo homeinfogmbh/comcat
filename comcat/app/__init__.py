fa41caff1bc9c6b41f54baff60a36ed5a1b63dca
Richard Neumann <mail@richard-neumann.de>
2020-04-16 13:36:49 +0200

    Refac.

--- comcat/app/__init__.py
+++ comcat/app/__init__.py
@@ -4,7 +4,6 @@ from comcat.app.common import APPLICATION
 from comcat.app.damage_report import list_damage_reports, submit_damage_report
 from comcat.app.file import get_file
 from comcat.app.local_news import get_local_news_articles, get_local_news_image
-from comcat.app.login import login
 from comcat.app.lpt import get_departures
 from comcat.app.presentation import get_presentation
 
@@ -19,6 +18,5 @@ APPLICATION.route('/local-news', methods=['GET'])(get_local_news_articles)
 APPLICATION.route(
     '/local-news/<int:article_id>/<int:image_id>', methods=['GET']
 )(get_local_news_image)
-APPLICATION.route('/login', methods=['POST'])(login)
 APPLICATION.route('/ltp', methods=['GET'])(get_departures)
 APPLICATION.route('/presentation', methods=['GET'])(get_presentation)
