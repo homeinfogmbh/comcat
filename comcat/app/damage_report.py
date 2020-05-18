b34d976d76d81fa3e0b46c48eef2ec1b28480ef1
Richard Neumann <mail@richard-neumann.de>
2020-05-18 12:34:56 +0200

    Refactored damage report endpoint.

--- comcat/app/damage_report.py
+++ comcat/app/damage_report.py
@@ -3,11 +3,10 @@
 from authlib.integrations.flask_oauth2 import current_token
 from flask import request
 
-from comcatlib import REQUIRE_OAUTH
-from comcatlib import UserDamageReport
+from comcatlib import REQUIRE_OAUTH, User, UserDamageReport
 from comcatlib.messages import NO_ADDRESS_CONFIGURED
-from damage_report import DamageReport
-from wsgilib import JSON
+from damage_report import DamageReport, Attachment
+from wsgilib import JSON, JSONMessage
 
 
 __all__ = ['list_damage_reports', 'submit_damage_report']
@@ -15,14 +14,60 @@ __all__ = ['list_damage_reports', 'submit_damage_report']
 
 DENIED_FIELDS = {'address', 'timestamp', 'checked'}
 
+DAMAGE_REPORT_SUBMITTED = JSONMessage('Damage report submitted.', status=201)
+NO_SUCH_ATTACHMENT = JSONMessage('No such attachment.', status=404)
+NO_SUCH_DAMAGE_REPORT = JSONMessage('No such damage report.', status=404)
+
+
+def _get_damage_reports():
+    """Yields all damage reports of the user."""
+
+    select = DamageReport.select().join(UserDamageReport).join(User)
+    condition_owner = UserDamageReport.submitter == current_token.user
+    condition_other = UserDamageReport.submitter != current_token.user
+    condition_other &= UserDamageReport.private == 0
+    condition_other &= User.tenement == current_token.user.tenement
+    condition = condition_owner | condition_other
+    return select.where(condition)
+
+
+def _get_damage_report(report_id):
+    """Returns a damage report."""
+
+    try:
+        return _get_damage_reports().where(DamageReport.id == report_id).get()
+    except DamageReport.DoesNotExist:
+        raise NO_SUCH_DAMAGE_REPORT
+
+
+def _get_attachment(report_id, attachment_id):
+    """Returns the respective attachment."""
+
+    select = Attachment.select().join(DamageReport).join(UserDamageReport)
+    condition = Attachment.id == attachment_id
+    condition &= DamageReport.id == report_id
+    condition &= UserDamageReport.user == current_token.user
+
+    try:
+        return select.where(condition).get()
+    except Attachment.DoesNotExist:
+        raise NO_SUCH_ATTACHMENT
+
 
 @REQUIRE_OAUTH('comcat')
 def list_damage_reports():
     """Returns a list of sent damage report."""
 
-    select = UserDamageReport.user == current_token.user
-    damage_reports = DamageReport.select().join(UserDamageReport).where(select)
-    return JSON([damage_report.to_dict() for damage_report in damage_reports])
+    damage_reports = _get_damage_reports()
+    return JSON([report.to_dict() for report in damage_reports])
+
+
+@REQUIRE_OAUTH('comcat')
+def get_damage_report(report_id):
+    """Returns a damage report."""
+
+    damage_report = _get_damage_report(report_id)
+    return JSON(damage_report.to_json())
 
 
 @REQUIRE_OAUTH('comcat')
@@ -42,4 +87,12 @@ def submit_damage_report():
         user=current_token.user, damage_report=damage_report
     )
     user_damage_report.save()
-    return ('Damage report submitted.', 201)
+    return DAMAGE_REPORT_SUBMITTED
+
+
+@REQUIRE_OAUTH('comcat')
+def get_attachment(report_id, attachment_id):
+    """Returns an image from the damage report."""
+
+    attachment = _get_attachment(report_id, attachment_id)
+    return attachment.bytes
