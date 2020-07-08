fe02b20be28991dbf5027404a6758447075b76c3
Richard Neumann <r.neumann@homeinfo.de>
2020-07-08 12:09:13 +0200

    Implemented chart endpoint.

--- comcat/app/__init__.py
+++ comcat/app/__init__.py
@@ -1,19 +1,20 @@
 """Smartphone app endpoints."""
 
+from comcat.app import charts
 from comcat.app import damage_report
 from comcat.app import file
 from comcat.app import local_news
 from comcat.app import lpt
-from comcat.app import presentation
 from comcat.app.common import APPLICATION
 
 
 __all__ = ['APPLICATION']
 
 
-ENDPOINTS = (
-    damage_report.ENDPOINTS + file.ENDPOINTS + local_news.ENDPOINTS
-    + lpt.ENDPOINTS + presentation.ENDPOINTS
+ENDPOINTS = sum(
+    (damage_report.ENDPOINTS, file.ENDPOINTS, local_news.ENDPOINTS,
+     lpt.ENDPOINTS),
+    charts.ENDPOINTS
 )
 
 
---
+++ comcat/app/charts.py
@@ -0,0 +1,54 @@
+"""ComCat app interface for charts."""
+
+from authlib.integrations.flask_oauth2 import current_token
+from peewee import JOIN
+
+from cmslib.orm.charts import BaseChart
+from cmslib.orm.content.group import GroupBaseChart
+from wsgilib import JSON
+
+from comcatlib.orm.content import UserBaseChart
+from comcatlib.orm.group import GroupMemberUser
+
+
+def user_groups(user):
+    """Yields all groups the given deployment is a member of."""
+
+    condition = GroupMemberUser.user == user
+
+    for gmu in GroupMemberUser.select().where(condition):
+        yield gmu.group
+        yield from gmu.group.parents
+
+
+def get_user_base_charts():
+    """Yields base charts, the current user has access to."""
+
+    condition = UserBaseChart.user == current_token.user
+    condition |= GroupBaseChart.group << set(user_groups(current_token.user))
+    return BaseChart.select().join(UserBaseChart, JOIN.LEFT_OUTER).switch(
+        BaseChart).join(GroupBaseChart, JOIN.LEFT_OUTER).where(condition)
+
+
+def get_base_chart(ident):
+    """Returns a base chart."""
+
+    return get_user_base_charts().where(BaseChart.id == ident)
+
+
+def list_():
+    """Lists available charts."""
+
+    return JSON([bc.chart.to_json() for bc in get_user_base_charts()])
+
+
+def get(ident):
+    """Returns a chart by it's base chart ID."""
+
+    return JSON(get_base_chart(ident).chart.to_json())
+
+
+ENDPOINTS = (
+    (['GET'], '/charts', list_),
+    (['GET'], '/chart/<int:ident>', get)
+)
