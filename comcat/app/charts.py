6c0e218aa5f5155d279c82f4e98246d5d879f187
Richard Neumann <r.neumann@homeinfo.de>
2020-07-10 10:20:11 +0200

    Filtered trashed charts and removed schedules in JSON view.

--- comcat/app/charts.py
+++ comcat/app/charts.py
@@ -1,5 +1,7 @@
 """ComCat app interface for charts."""
 
+from contextlib import suppress
+
 from authlib.integrations.flask_oauth2 import current_token
 from peewee import JOIN
 
@@ -31,6 +33,7 @@ def get_user_base_charts():
 
     condition = UserBaseChart.user == current_token.user
     condition |= GroupBaseChart.group << set(user_groups(current_token.user))
+    condition &= BaseChart.trashed == 0
     return BaseChart.select().join(UserBaseChart, JOIN.LEFT_OUTER).switch(
         BaseChart).join(GroupBaseChart, JOIN.LEFT_OUTER).where(condition)
 
@@ -46,6 +49,10 @@ def jsonify_base_chart(base_chart):
 
     json = base_chart.chart.to_json()
     json['base']['menus'] = [menu.menu.value for menu in get_menus(base_chart)]
+
+    with suppress(KeyError):    # Remove unneeded schedules.
+        del json['base']['schedule']
+
     return json
 
 
