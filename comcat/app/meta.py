c53466054ac6ed964d535844e3872d26f5554765
Richard Neumann <r.neumann@homeinfo.de>
2020-10-05 11:25:18 +0200

    Refac.

--- comcat/app/meta.py
+++ comcat/app/meta.py
@@ -1,21 +1,46 @@
 """Meta backend."""
 
+from io import BytesIO
+
+from flask import request
+from qrcode import make
+from wsgilib import Bytes
+
 from comcatlib import REQUIRE_OAUTH, USER, InitializationNonce
 
 
 __all__ = ['ENDPOINTS']
 
 
+def _get_nonce():
+    """Returns an existing Nonce or creates a new one."""
+
+    try:
+        return InitializationNonce.get(InitializationNonce.user == USER.id)
+    except InitializationNonce.DoesNotExist:
+        return InitializationNonce.add(user=USER.id)
+
+
 @REQUIRE_OAUTH('comcat')
 def generate_initialization_nonce():
     """Generates a new initialization nonce."""
 
-    try:
-        nonce = InitializationNonce.get(InitializationNonce.user == USER.id)
-    except InitializationNonce.DoesNotExist:
-        nonce = InitializationNonce.add(user=USER.id)
-
-    return nonce.uuid.hex
+    return _get_nonce().uuid.hex
 
 
-ENDPOINTS = [(['GET'], '/initnonce', generate_initialization_nonce)]
+@REQUIRE_OAUTH('comcat')
+def get_qr_code():
+    """Returns a QR code of the user's initialization nonce."""
+
+    format = request.args.get('format', 'png')  # pylint: disable=W0622
+    qrcode = make(_get_nonce().url)
+
+    with BytesIO() as buf:
+        qrcode.save(buf, format=format)
+        return Bytes(buf.read())
+
+
+ENDPOINTS = [
+    (['GET'], '/init/nonce', generate_initialization_nonce),
+    (['GET'], '/init/qrcode', get_qr_code)
+]
