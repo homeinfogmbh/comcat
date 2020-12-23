b3f6bab096c5105ff1481f6ba205b80978b83a06
Richard Neumann <mail@richard-neumann.de>
2020-12-23 15:05:14 +0100

    Refactored Routes setting.

--- comcat/his/__init__.py
+++ comcat/his/__init__.py
@@ -8,7 +8,9 @@ from comcat.his import content, group, menu, tenement, user
 __all__ = ['APPLICATION']
 
 
-ROUTES = [*content.ROUTES, *group.ROUTES, *menu.ROUTES, *tenement.ROUTES,
-          *user.ROUTES]
 APPLICATION = Application('comcat')
-APPLICATION.add_routes(ROUTES)
+APPLICATION.add_routes(content.ROUTES)
+APPLICATION.add_routes(group.ROUTES)
+APPLICATION.add_routes(menu.ROUTES)
+APPLICATION.add_routes(tenement.ROUTES)
+APPLICATION.add_routes(user.ROUTES)
