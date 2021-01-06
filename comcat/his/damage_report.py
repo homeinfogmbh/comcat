5e9ee5acb1454a2e024e0b74311eebc30263f80d
Richard Neumann <r.neumann@homeinfo.de>
2021-01-06 16:12:49 +0100

    Fix.

--- comcat/his/damage_report.py
+++ comcat/his/damage_report.py
@@ -11,10 +11,7 @@ from comcat.his.functions import get_user_damage_reports
 def list_() -> JSON:
     """Lists damage reports from ComCat accounts."""
 
-    return JSON([
-        report.to_json(attachments=True)
-        for report in get_user_damage_reports()
-    ])
+    return JSON([report.to_json() for report in get_user_damage_reports()])
 
 
 ROUTES = [('GET', '/user_damage_report', list_)]
