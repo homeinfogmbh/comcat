38843e05267cfc84acc81d153d850908d36578dc
Richard Neumann <mail@richard-neumann.de>
2021-01-22 15:36:15 +0100

    Added error handlers.

--- comcat/his/__init__.py
+++ comcat/his/__init__.py
@@ -3,15 +3,19 @@
 from his import Application
 
 from comcat.his import content, damage_report, group, menu, tenement, user
+from comcat.his.errors import ERRORS
 
 
 __all__ = ['APPLICATION']
 
 
 APPLICATION = Application('comcat')
-APPLICATION.add_routes(content.ROUTES)
-APPLICATION.add_routes(damage_report.ROUTES)
-APPLICATION.add_routes(group.ROUTES)
-APPLICATION.add_routes(menu.ROUTES)
-APPLICATION.add_routes(tenement.ROUTES)
-APPLICATION.add_routes(user.ROUTES)
+ROUTES = (
+    *content.ROUTES, *damage_report.ROUTES, *group.ROUTES, *menu.ROUTES,
+    *tenement.ROUTES, *user.ROUTES
+)
+APPLICATION.add_routes(ROUTES)
+
+
+for exception, handler in ERRORS.items():
+    APPLICATION.register_error_handler(exception, handler)
