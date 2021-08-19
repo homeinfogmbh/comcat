2ad2c69c0300bb4e835f00371dbd080f8e0e72bf
Richard Neumann <mail@richard-neumann.de>
2021-08-19 13:22:39 +0200

    Use functions from comcatlib

--- comcat/app/tenant2tenant.py
+++ comcat/app/tenant2tenant.py
@@ -1,138 +1,36 @@
 """Tenant-to-tenant endpoint."""
 
-from datetime import datetime
-
 from flask import request
-from peewee import JOIN, ModelCursorWrapper, ModelSelect
 
-from tenant2tenant import Configuration
-from tenant2tenant import TenantMessage
-from tenant2tenant import Visibility
 from wsgilib import JSON, JSONMessage
 
-from comcatlib import ADDRESS
-from comcatlib import CUSTOMER
-from comcatlib import DATABASE
 from comcatlib import REQUIRE_OAUTH
 from comcatlib import USER
-from comcatlib import User
-from comcatlib import UserTenantMessage
-
-from comcat.app.functions import is_own_message, get_sender_name
+from comcatlib import add_user_tenant_message
+from comcatlib import get_deletable_tenant_message
+from comcatlib import get_tenant_messages
+from comcatlib import jsonify_tenant_message
 
 
 __all__ = ['ENDPOINTS']
-
-
-def _select_messages() -> ModelSelect:
-    """Yields the tenant-to-tenant messages the current user may access."""
-
-    select = TenantMessage.select(
-        UserTenantMessage, User, cascade=True).join_from(
-        TenantMessage, UserTenantMessage, JOIN.LEFT_OUTER).join(
-        User, JOIN.LEFT_OUTER)
-
-    if USER.admin:
-        # Admins can see all tenant-to-tenant messages of their company.
-        return select.where(TenantMessage.customer == CUSTOMER.id)
-
-    condition = (
-        # Always allow own messages.
-        (UserTenantMessage.user == USER.id)
-        | (
-            # Show messages of the same customer
-            # under the following conditions.
-            (TenantMessage.customer == CUSTOMER.id)
-            # Only show released messages.
-            & (TenantMessage.released == 1)
-            & (
-                (
-                    # If the visibility is set to customer-wide,
-                    # show all those entries of the same customer.
-                    TenantMessage.visibility == Visibility.CUSTOMER
-                ) | (
-                    # If the visibility is restricted to tenement, only
-                    # show entries of the same address.
-                    (TenantMessage.visibility == Visibility.TENEMENT)
-                    & (TenantMessage.address == ADDRESS.id)
-                )
-            )
-        )
-    )
-    return select.where(condition)
-
-
-def _get_messages() -> ModelCursorWrapper:
-    """Yields messages."""
-
-    return _select_messages().execute(DATABASE)
-
-
-def _get_deletable_message(ident: int) -> ModelSelect:
-    """Returns a tenant-to-tenant message
-    that the current user may delete.
-    """
-
-    condition = TenantMessage.customer == CUSTOMER.id
-    condition &= TenantMessage.id == ident
-
-    if USER.admin:
-        return TenantMessage.select().where(condition).get()
-
-    condition &= UserTenantMessage.user == USER.id
-    select = TenantMessage.select().join(UserTenantMessage)
-    return select.where(condition).get()
-
-
-def _add_message() -> TenantMessage:
-    """Adds a tenant message."""
-
-    message = request.json['message']
-    tenant_message = TenantMessage.add(CUSTOMER.id, ADDRESS.id, message)
-    tenant_message.subject = request.json.get('subject')
-    visibility = request.json.get('visibility')
-
-    if visibility:
-        tenant_message.visibility = Visibility(visibility)
-    else:
-        tenant_message.visibility = Visibility.TENEMENT
-
-    configuration = Configuration.for_customer(CUSTOMER.id)
-
-    if configuration.auto_release:
-        tenant_message.released = True
-        tenant_message.start_date = now = datetime.now()
-        tenant_message.end_date = now + configuration.release_time
-
-    tenant_message.save()
-    return tenant_message
-
-
-def _jsonify(message: TenantMessage, *args, **kwargs) -> dict:
-    """Converts a tenant message into a JSON-ish dict."""
-
-    json = message.to_json(*args, **kwargs)
-    json['own'] = is_own_message(message)
-    json['sender'] = get_sender_name(message)
-    return json
 
 
 @REQUIRE_OAUTH('comcat')
 def list_() -> JSON:
     """Lists all tenant-to-tenant messages."""
 
-    return JSON([_jsonify(msg) for msg in _get_messages()])
+    user = USER._get_current_object()   # pylint: disable=W0212
+    tenant_messages = get_tenant_messages(user)
+    return JSON([jsonify_tenant_message(msg) for msg in tenant_messages])
 
 
 @REQUIRE_OAUTH('comcat')
 def post() -> JSONMessage:
     """Adds a new tenant-to-tenant message."""
 
-    tenant_message = _add_message()
-    user_tenant_message = UserTenantMessage(
-        tenant_message=tenant_message, user=USER.id)
-    user_tenant_message.save()
-    return JSONMessage('Tenant message added.', id=tenant_message.id,
+    user = USER._get_current_object()   # pylint: disable=W0212
+    user_tenant_message = add_user_tenant_message(request.json, user)
+    return JSONMessage('Tenant message added.', id=user_tenant_message.id,
                        status=201)
 
 
@@ -140,7 +38,8 @@ def post() -> JSONMessage:
 def delete(ident: int) -> JSONMessage:
     """Deletes a tenant-to-tenant message."""
 
-    _get_deletable_message(ident).delete_instance()
+    user = USER._get_current_object()   # pylint: disable=W0212
+    get_deletable_tenant_message(user, ident).delete_instance()
     return JSONMessage('Tenant message deleted.', status=200)
 
 
