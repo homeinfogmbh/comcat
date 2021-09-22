57dc80a229cfffba02d6e9d9269fbf2b9b5c7e1f
Richard Neumann <mail@richard-neumann.de>
2021-09-22 14:42:11 +0200

    Add HIS interface

--- comcat/his/__init__.py
+++ comcat/his/__init__.py
@@ -5,6 +5,7 @@ from his import Application
 from comcat.his import content
 from comcat.his import damage_report
 from comcat.his import group
+from comcat.his import marketplace
 from comcat.his import menu
 from comcat.his import tenant2tenant
 from comcat.his import tenement
@@ -17,8 +18,8 @@ __all__ = ['APPLICATION']
 
 APPLICATION = Application('comcat')
 ROUTES = (
-    *content.ROUTES, *damage_report.ROUTES, *group.ROUTES, *menu.ROUTES,
-    *tenant2tenant.ROUTES, *tenement.ROUTES, *user.ROUTES
+    *content.ROUTES, *damage_report.ROUTES, *group.ROUTES, *marketplace.ROUTES,
+    *menu.ROUTES, *tenant2tenant.ROUTES, *tenement.ROUTES, *user.ROUTES
 )
 APPLICATION.add_routes(ROUTES)
 
