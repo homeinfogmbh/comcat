8391b84e36b2f83ccf46463c1d7f2e5d82c18556
Richard Neumann <mail@richard-neumann.de>
2021-09-23 09:55:30 +0200

    Remove faulty import

--- comcat/app/damage_report/__init__.py
+++ comcat/app/damage_report/__init__.py
@@ -1,7 +1,6 @@
 """Damage report endpoints."""
 
 from comcat.app.damage_report import attachment, damage_report
-from comcat.app.damage_report.errors import ERRORS
 
 
 __all__ = ['ENDPOINTS', 'ERRORS']
