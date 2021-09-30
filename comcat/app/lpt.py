4988fe278c37417cf936ce43ab0f024cd1fd1a3a
Richard Neumann <mail@richard-neumann.de>
2021-09-30 11:35:21 +0200

    Add endpoint for current geo coordinates base LPT

--- comcat/app/lpt.py
+++ comcat/app/lpt.py
@@ -1,7 +1,9 @@
 """Local public transport endpoints."""
 
+from flask import request
+
 from comcatlib import REQUIRE_OAUTH, TENEMENT
-from lptlib import get_departures as _get_departures
+from lptlib import get_departures
 from wsgilib import JSON
 
 
@@ -9,12 +11,26 @@ __all__ = ['ROUTES']
 
 
 @REQUIRE_OAUTH('comcat')
-def get_departures() -> JSON:
-    """Returns the departures."""
+def get_home_departures() -> JSON:
+    """Returns the departures of the tenenemt address."""
 
-    stops, source = _get_departures(TENEMENT.address)
+    stops, source = get_departures(TENEMENT.address)
     stops = [stop.to_json() for stop in stops]
     return JSON({'source': source, 'stops': stops})
 
 
-ROUTES = [(['GET'], '/lpt', get_departures)]
+@REQUIRE_OAUTH('comcat')
+def get_current_departures() -> JSON:
+    """Returns the departures of the given geo coordinates."""
+
+    latitude = float(request.json['latitude'])
+    longitude = float(request.json['longitude'])
+    stops, source = get_departures((latitude, longitude))
+    stops = [stop.to_json() for stop in stops]
+    return JSON({'source': source, 'stops': stops})
+
+
+ROUTES = [
+    (['GET'], '/lpt/home', get_home_departures),
+    (['POST'], '/lpt/current', get_current_departures)
+]
