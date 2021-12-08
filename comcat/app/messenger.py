302a4e8dde7fb11c8aeebca1626dc3ac19388c88
Richard Neumann <r.neumann@homeinfo.de>
2021-12-08 13:28:43 +0100

    Fix for new ccmessenger API

--- comcat/app/messenger.py
+++ comcat/app/messenger.py
@@ -2,13 +2,10 @@
 
 from flask import request
 
-from ccmessenger import get_own_attachment
-from ccmessenger import get_own_message
-from ccmessenger import get_own_messages
-from ccmessenger import get_user_message
+from ccmessenger import get_attachment, get_user_message, get_user_messages
 from ccmessenger import Attachment
 from ccmessenger import Message
-from comcatlib import REQUIRE_OAUTH, TENEMENT, USER
+from comcatlib import REQUIRE_OAUTH, USER
 from filedb import File
 from wsgilib import Binary, JSON, JSONMessage
 
@@ -28,14 +25,14 @@ ERRORS = {
 def list_messages() -> JSON:
     """Lists messages of the current user."""
 
-    return JSON([message.to_json() for message in get_own_messages(USER.id)])
+    return JSON([message.to_json() for message in get_user_messages(USER.id)])
 
 
 @REQUIRE_OAUTH('comcat')
-def get_attachment(ident: int) -> Binary:
+def get_attachment_(ident: int) -> Binary:
     """Returns attachment data."""
 
-    return Binary(get_own_attachment(ident, USER.id).file.bytes)
+    return Binary(get_attachment(ident, user=USER.id).file.bytes)
 
 
 @REQUIRE_OAUTH('comcat')
@@ -43,7 +40,7 @@ def add_message() -> JSONMessage:
     """Adds a new message."""
 
     if (reply_to := request.json.get('replyTo')) is not None:
-        reply_to = get_user_message(reply_to, TENEMENT.customer, user=USER.id)
+        reply_to = get_user_message(reply_to, user=USER.id)
 
     message = Message(parent=reply_to, user=USER.id, text=request.json['text'])
     message.save()
@@ -55,7 +52,7 @@ def add_attachment(message: int) -> JSONMessage:
     """Adds a new attachment."""
 
     attachment = Attachment(
-        message=get_own_message(message, USER.id),
+        message=get_user_message(message, USER.id, own=True),
         file=File.from_bytes(request.get_data(), save=True)
     )
     attachment.save()
@@ -66,14 +63,14 @@ def add_attachment(message: int) -> JSONMessage:
 def delete_attachment(ident: int) -> JSONMessage:
     """Deletes an attachment."""
 
-    get_own_attachment(ident, USER.id).delete_instance()
+    get_attachment(ident, USER.id, own=True).delete_instance()
     return JSONMessage('Attachment deleted.', status=200)
 
 
 ROUTES = [
     (['GET'], '/messenger/message', list_messages),
     (['POST'], '/messenger/message', add_message),
-    (['GET'], '/messenger/attachment/<int:ident>', get_attachment),
+    (['GET'], '/messenger/attachment/<int:ident>', get_attachment_),
     (['POST'], '/messenger/attachment/<int:ident>', add_attachment),
     (['DELETE'], '/messenger/attachment/<int:ident>', delete_attachment)
 ]
