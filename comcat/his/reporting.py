6de1f627ba8b070734d00e1b43f4d0acea27ac49
Richard Neumann <r.neumann@homeinfo.de>
2022-07-15 12:21:14 +0200

    Add listing endpoints

--- comcat/his/reporting.py
+++ comcat/his/reporting.py
@@ -4,10 +4,62 @@ from comcatlib import User
 from his import CUSTOMER, authenticated, authorized
 from mdb import Tenement
 from reportlib import OfferReport, TopicReport, ResponseReport, UserEventReport
-from wsgilib import JSONMessage
+from wsgilib import JSON, JSONMessage
 
 
 __all__ = ['ROUTES']
+
+
+@authenticated
+@authorized('comcat')
+def list_offer_reports() -> JSON:
+    """List offer reports."""
+
+    return JSON([
+        report.to_json() for report
+        in OfferReport.select().join(User).join(Tenement).where(
+            Tenement.customer == CUSTOMER.id
+        )
+    ])
+
+
+@authenticated
+@authorized('comcat')
+def list_topic_reports() -> JSON:
+    """List topic reports."""
+
+    return JSON([
+        report.to_json() for report
+        in TopicReport.select().join(User).join(Tenement).where(
+            Tenement.customer == CUSTOMER.id
+        )
+    ])
+
+
+@authenticated
+@authorized('comcat')
+def list_response_reports() -> JSON:
+    """List offer reports."""
+
+    return JSON([
+        report.to_json() for report
+        in ResponseReport.select().join(User).join(Tenement).where(
+            Tenement.customer == CUSTOMER.id
+        )
+    ])
+
+
+@authenticated
+@authorized('comcat')
+def list_user_event_reports() -> JSON:
+    """List user event reports."""
+
+    return JSON([
+        report.to_json() for report
+        in UserEventReport.select().join(User).join(Tenement).where(
+            Tenement.customer == CUSTOMER.id
+        )
+    ])
 
 
 @authenticated
@@ -59,6 +111,10 @@ def delete_user_event_report(ident: int) -> JSONMessage:
 
 
 ROUTES = [
+    ('GET', '/reporting/offer', list_offer_reports),
+    ('GET', '/reporting/topic', list_topic_reports),
+    ('GET', '/reporting/response', list_response_reports),
+    ('GET', '/reporting/event', list_user_event_reports),
     ('DELETE', '/reporting/offer/<int:ident>', delete_offer_report),
     ('DELETE', '/reporting/topic/<int:ident>', delete_topic_report),
     ('DELETE', '/reporting/response/<int:ident>', delete_response_report),
